9f3b6da13672bde92e8df9ce10f1633d8d038349
Sergei Grichine <msghub@hotmail.com>
2025-10-24 15:45:10 -0500

    Update FOC_vel_ctrl_HALL_Teensy_40.ino

--- Arduino/Sketchbook/FOC_vel_ctrl_HALL_Teensy_40/FOC_vel_ctrl_HALL_Teensy_40.ino
+++ Arduino/Sketchbook/FOC_vel_ctrl_HALL_Teensy_40/FOC_vel_ctrl_HALL_Teensy_40.ino
@@ -296,7 +296,9 @@ void loop() {
   // velocity, position or voltage (defined in motor.controller)
   // this function can be run at much lower frequency than loopFOC() function
   // You can also use motor.move() and set the motor.target in the code
-  //motor.move(target_velocity);
+  //
+  // Note: comment this out if you want to rotate wheel by hand
+  motor.move(target_velocity);
 
   // function intended to be used with serial plotter to monitor motor variables
   // significantly slowing the execution down!!!!
